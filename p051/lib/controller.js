9f46d91b7c5a34eb660e366190c08036cd6692fc
igoryen <igor.yentaltsev@gmail.com>
2015-04-05 19:51:44 -0400

    p51 example: finished

--- p051/lib/controller.js
+++ p051/lib/controller.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+angular.module('notesApp', [])
+  .controller('MainCtrl', [function(){
+    var self = this;
+    self.submit = function(){
+      console.log('User clicked submit with ', self.user);
+    }
+  }]);
